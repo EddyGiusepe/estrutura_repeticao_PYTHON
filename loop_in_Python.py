52f12d1502ae006f2902a112e6204a04e6ac74be
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-10-10 16:33:05 -0300

    criando um loop com for

---
+++ loop_in_Python.py
@@ -0,0 +1,13 @@
+'''
+Data Scientist: Eddy Giusepe Chirinos Isidro
+
+Este script está baseado no seguinte vídeo: https://www.youtube.com/watch?v=jtVYx9W0dgc
+'''
+
+print("##########################################")
+print("Criamos um loop no python com for, assim: ")
+print("##########################################")
+
+for index in range(1, 5, 1):
+    print(index)
+
